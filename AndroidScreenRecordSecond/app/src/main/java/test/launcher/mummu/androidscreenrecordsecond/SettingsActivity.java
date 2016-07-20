b145ceb093bce22289757f1f7415012c73dc8914
muhammed <muhammed.thasneem@yahoo.com>
2016-07-20 11:56:50 +0530

    settings design done

--- AndroidScreenRecordSecond/app/src/main/java/test/launcher/mummu/androidscreenrecordsecond/SettingsActivity.java
+++ AndroidScreenRecordSecond/app/src/main/java/test/launcher/mummu/androidscreenrecordsecond/SettingsActivity.java
@@ -3,14 +3,41 @@ package test.launcher.mummu.androidscreenrecordsecond;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 /**
  * Created by muhammed on 19/7/16.
  */
 public class SettingsActivity extends AppCompatActivity {
+    private Toolbar mToolbar;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         setContentView(R.layout.activity_settings);
+
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_arrow_back_white_dp);
+
         super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
